8c27f4a5081829141f3274e7f6199fa59f766c0e
Murugesh Kumar K <master4uall@gmail.com>
2019-07-05 17:38:08 -0400

    create a new component manually

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'app-server', //tag name to use this component, should be a string
+   templateUrl: './server.component.html' //external file that is template for this file
+}
+)
+export class ServerComponent {
+    //normal typescript class - so far
+}
