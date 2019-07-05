8f73f98a76e001b15a40db97b51eaa3dfc027e8c
Murugesh Kumar K <master4uall@gmail.com>
2019-07-05 17:51:21 -0400

    Added new component named servers using CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';//dont have to add extension (.ts)
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';//dont have to add extension (.ts)
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
