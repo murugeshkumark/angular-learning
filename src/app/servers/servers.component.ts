128646d93967f0cfa409d641c5c3f66b2dfe8b1f
Murugesh Kumar K <master4uall@gmail.com>
2019-07-06 19:37:23 -0400

    Modified to demo all type of data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer= false;
   serverCreationStatus= 'No Server was created!';
-  serverName= '';
+  serverName= 'Test';
 
   constructor() { 
     setTimeout(()=>{
@@ -22,7 +22,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
-    this.serverCreationStatus ='Server was created';
+    this.serverCreationStatus ='Server was created! Name is '+ this.serverName;
   }
 
   onUpdateServerName(event: Event){
