8167c308afd6bfce8cc5fc0c1d0a432ce542624d
Murugesh Kumar K <master4uall@gmail.com>
2019-07-06 20:10:19 -0400

    Modified to demo ngClass Directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-   selector: 'app-server', //tag name to use this component, should be a string
-   templateUrl: './server.component.html' //external file that is template for this file
+    selector: 'app-server', //tag name to use this component, should be a string
+    templateUrl: './server.component.html', //external file that is template for this file,
+    styles: [`
+   .online {
+       color : white;
+   }
+   `]
 }
 )
 export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = 'offline';
 
-    constructor(){
+    constructor() {
         this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
     }
 
-    getServerStatus(){
+    getServerStatus() {
         return this.serverStatus;
     }
 
-    getColor(){
+    getColor() {
         return this.serverStatus === 'online' ? 'green' : 'red';
     }
 
